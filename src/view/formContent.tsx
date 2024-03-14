d30115ace1b3f540326477b5ad65d5d5f2aaea35
young <55731744@qq.com>
2024-03-14 11:22:54 +0800

    lint fix

--- src/view/formContent.tsx
+++ src/view/formContent.tsx
@@ -1,32 +1,31 @@
-import { Form, Button, Input} from 'antd-mobile'
+import {Form, Button, Input} from 'antd-mobile'
 
 function FormContent() {
-    const [form] = Form.useForm()
+  const [form] = Form.useForm()
 
-    const submit = () => {
-        // console.log('form', form);
-    }
-    return (
-        <Form form={form} layout="horizontal" footer={
-            <Button block type='submit' color='primary' size='small'>
-              确定
-            </Button>
-          }>
-            <Form.Item
-                name='value'
-                label='value'
-                rules={[{ required: true, message: 'value不能为空' }]}
-            >
-                <Input placeholder='' />
-            </Form.Item>
-            <Form.Item
-                name='explain'
-                label='explain'
-                rules={[{ required: false, message: '姓名不能为空' }]}
-            >
-                <Input placeholder='' />
-            </Form.Item>
-        </Form>
-    )
+
+  return (
+    <Form form={form} layout="horizontal" footer={
+      <Button block type='submit' color='primary' size='small'>
+        确定
+      </Button>
+    }>
+      <Form.Item
+        name='value'
+        label='value'
+        rules={[{required: true, message: 'value不能为空'}]}
+      >
+        <Input placeholder=''/>
+      </Form.Item>
+      <Form.Item
+        name='explain'
+        label='explain'
+        rules={[{required: false, message: '姓名不能为空'}]}
+      >
+        <Input placeholder=''/>
+      </Form.Item>
+    </Form>
+  )
 }
+
 export default FormContent
--- src/view/layout.tsx
+++ src/view/layout.tsx
@@ -7,84 +7,66 @@ import FormContent from './formContent';
 
 
 function Layout() {
-    const [visible, setVisible] = useState<boolean>(false)
-    const [list, setList] = useState(vocabularyArray)
-    const changeVisible = (index: number) => {
-        const updateList = [...list]
-        updateList.forEach((item, i) => {
-            if (i === index) item.explainVisible = !item.explainVisible
-        })
-        setList(updateList)
+  const [visible, setVisible] = useState<boolean>(false)
+  const [list, setList] = useState(vocabularyArray)
+  const changeVisible = (index: number) => {
+    const updateList = [...list]
+    updateList.forEach((item, i) => {
+      if (i === index) item.explainVisible = !item.explainVisible
+    })
+    setList(updateList)
+  }
+  const tabChange = (key: string) => {
+    switch (key) {
+      case '1':
+        setList(vocabularyArray)
+        break;
+      case '2':
+        setList(vocabularyArray2)
+        break;
     }
-    const tabChange = (key: string) => {
-        switch (key) {
-            case '1':
-                setList(vocabularyArray)
-                break;
-            case '2':
-                setList(vocabularyArray2)
-                break;
-        }
-    }
-    return (
-        <div className="layout">
-            <Modal
-                forceRender={true}
-                content={FormContent()}
-                visible={visible}
-                showCloseButton
-                onClose={() => {
-                    setVisible(false)
-                }}
-                closeOnAction>
-            </Modal>
-            <Tabs onChange={tabChange}>
-                <Tabs.Tab key="1" title={"G1"}>
-                    <ul className="list">
-                        {list.map((item, index) =>
-                            <li style={{fontWeight: item.bold ? 700 : 400}} key={item.label}>
-                                <label htmlFor="">{item.label}</label>
-                                {/*{item.explainVisible ? <span style={{marginLeft: '10px'}}>{item.explain}</span> : null}*/}
-                                <span style={{
-                                    marginLeft: '10px',
-                                    display: item.explainVisible ? 'inline' : 'none'
-                                }}>{item.explain}</span>
-                                <Button onClick={() => changeVisible(index)} color='primary' className={'addBtn'}>
-                                    {item.explainVisible ? '隐藏' : '显示'}
-                                </Button>
-
-                            </li>
-                        )
-                        }
-                    </ul>
-                </Tabs.Tab>
-                <Tabs.Tab key="2" title={"G2"}>
-                    <ul className="list">
-                        {list.map((item, index) =>
-                            <li style={{fontWeight: item.bold ? 700 : 400}} key={item.label}>
-                                <label htmlFor="">{item.label}</label>
-                                {/*{item.explainVisible ? <span style={{marginLeft: '10px'}}>{item.explain}</span> : null}*/}
-                                <span style={{
-                                    marginLeft: '10px',
-                                    display: item.explainVisible ? 'inline' : 'none'
-                                }}>{item.explain}</span>
-                                <Button onClick={() => changeVisible(index)} color='primary' className={'addBtn'}>
-                                    {item.explainVisible ? '隐藏' : '显示'}
-                                </Button>
-
-                            </li>
-                        )
-                        }
-                    </ul>
-                </Tabs.Tab>
-                {/*<Tabs.Tab key={Math.random()} title={"G2"}></Tabs.Tab>*/}
-                {/*<Tabs.Tab key={Math.random()} title={"G2"}></Tabs.Tab>*/}
-                {/*<Tabs.Tab key={Math.random()} title={"G2"}></Tabs.Tab>*/}
-                {/*<Tabs.Tab key={Math.random()} title={"G2"}></Tabs.Tab>*/}
-                {/*<Tabs.Tab key={Math.random()} title={"G2"}></Tabs.Tab>*/}
-            </Tabs>
-        </div>
-    )
+  }
+  const tabItem: Array<{ key: string; title: string }> = [
+    {key: '1', title: 'G1'},
+    {key: '2', title: 'G2'}
+  ]
+  return (
+    <>
+      <div className="layout">
+        <Modal
+          forceRender={true}
+          content={<FormContent/>}
+          visible={visible}
+          showCloseButton
+          onClose={() => {
+            setVisible(false)
+          }}
+          closeOnAction>
+        </Modal>
+        <Tabs onChange={tabChange}>
+          {tabItem.map(tab =>
+            <Tabs.Tab title={tab.title} key={tab.key}>
+              <ul className="list">
+                {list.map((item, index) =>
+                  <li style={{fontWeight: item.bold ? 700 : 400}} key={item.label}>
+                    <label htmlFor="">{item.label}</label>
+                    {/*{item.explainVisible ? <span style={{marginLeft: '10px'}}>{item.explain}</span> : null}*/}
+                    <span style={{
+                      marginLeft: '10px',
+                      display: item.explainVisible ? 'inline' : 'none'
+                    }}>{item.explain}</span>
+                    <Button onClick={() => changeVisible(index)} color='primary'
+                            className={'addBtn'}>
+                      {item.explainVisible ? '隐藏' : '显示'}
+                    </Button>
+                  </li>)}
+              </ul>
+            </Tabs.Tab>
+          )}
+        </Tabs>
+      </div>
+    </>
+  )
 }
 
 export default Layout
