c8fa640ef8246beba214fd3893aaa85c88f92fe2
young <55731744@qq.com>
2024-04-24 17:04:32 +0800

    添加post-css的ts声明

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@
 import Layout from './view/layout'
 import './App.css'
 import React from "react";
-
+import hala from 'postcss-px-to-viewport'
 const App: React.FC = () =>{
+  console.log(hala);
   return (
     <>
       <Layout></Layout>
