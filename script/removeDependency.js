62205d3f1b9d41d398bde1c072a7be4ea8f5ebbb
young <55731744@qq.com>
2024-04-24 15:08:37 +0800

    添加自动化脚本

---
+++ script/removeDependency.js
@@ -0,0 +1,36 @@
+import readline from "readline";
+
+import {exec} from "child_process";
+
+
+// 创建readline接口
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log(process.env)
+rl.question('Enter the name of the dependency to remove: ', packageName => {
+    if (!packageName) {
+        console.log('No package name provided. Exiting...');
+        rl.close();
+        return;
+    }
+
+    // 运行npm卸载命令
+    exec(`npm uninstall ${packageName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            rl.close();
+            return;
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            rl.close();
+            return;
+        }
+
+        console.log(`Package '${packageName}' has been removed.`);
+        console.log(stdout);
+        rl.close();
+    });
+});
