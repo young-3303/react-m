469d8df9b47738ec4d596120e98f6132da7377f4
young <55731744@qq.com>
2026-03-26 00:07:48 +0800

    更新结构v7

---
+++ src/libs/vocabulary7.ts
@@ -0,0 +1,34 @@
+
+const vocabularyArray7 = [
+    { label: "obedient", bold: false, explain: "adj. 服从的；顺从的" },
+    { label: "considerable", bold: false, explain: "adj. 相当大的；相当多的" },
+    { label: "convenient", bold: false, explain: "adj. 方便的；便利的" },
+    { label: "credible", bold: false, explain: "adj. 可信的；可靠的" },
+    { label: "decent", bold: false, explain: "adj. 体面的；正派的；相当好的" },
+    { label: "decisive", bold: false, explain: "adj. 决定性的；果断的" },
+    { label: "delightful", bold: false, explain: "adj. 令人愉快的；讨人喜欢的" },
+    { label: "desirable", bold: false, explain: "adj. 理想的；值得拥有的；可取的" },
+    { label: "dominant", bold: false, explain: "adj. 占优势的；支配的；统治的" },
+    { label: "invaluable", bold: false, explain: "adj. 无价的，非常贵重的" },
+    { label: "judicial", bold: false, explain: "adj. 法庭的；公正的，明断的；审判上的" },
+    { label: "liberal", bold: false, explain: "adj. 慷慨的，大方的；丰富的，自由的" },
+    { label: "loyal", bold: false, explain: "adj. 忠诚的，忠贞的" },
+    { label: "modest", bold: false, explain: "adj. 端庄的，谦虚的，谦逊的；适度的" },
+    { label: "moral", bold: false, explain: "adj. 道德的，道义的，有道德的" },
+    { label: "noble", bold: false, explain: "adj. 高尚的" },
+    { label: "nutrient", bold: false, explain: "adj. 营养的，滋养的" },
+    { label: "optimistic", bold: false, explain: "adj. 乐观主义的" },
+    { label: "optimum", bold: false, explain: "adj. 最适宜的" },
+    { label: "patriotic", bold: false, explain: "adj. 爱国的" },
+    { label: "portable", bold: false, explain: "adj. 轻便的" },
+    { label: "positive", bold: false, explain: "adj. 确定的；肯定的；正面的，积极的；正的" },
+    { label: "precious", bold: false, explain: "adj. 珍贵的，贵重的" },
+    { label: "predominant", bold: false, explain: "adj. 卓越的；有力的；支配的" },
+    { label: "prominent", bold: false, explain: "adj. 突出的；显著的；杰出的，卓越的" },
+    { label: "promising", bold: false, explain: "adj. 有希望的，有前途的" },
+    { label: "proper", bold: false, explain: "adj. 适当的，恰当的；特有的，固有的" }
+]
+vocabularyArray7.forEach((item) => {
+    item.bold = false
+})
+export default vocabularyArray7
