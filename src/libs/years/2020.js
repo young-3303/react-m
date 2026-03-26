158c47247666a0fd1a0143de55c40f01ef8854a0
young <55731744@qq.com>
2026-03-26 23:35:10 +0800

    新增vac

---
+++ src/libs/years/2020.js
@@ -0,0 +1,79 @@
+/**
+ * @description There will be a ten-minute (interval) halfway through the performance.
+ * @param period
+ * @param time
+ * @param length
+ * @param break
+ */
+
+/**
+ * @description Most of Egypt’s inhabitants live in the Nile valley and delta, with the rest of the country (sparsely) populated.
+ * @param randomly
+ * @param thinly
+ * @param densely
+ * @param evenly
+ */
+
+/**
+ * @description The hotel owner was (authorized) to sell alcoholic drinks in his hotel.
+ * @param inclined
+ * @param prepared
+ * @param required
+ * @param licensed
+ */
+
+/**
+ * @description Your lawyer’s presence in the court room is (critical) since he may prevent you from being misdirected.
+ * @param impressive
+ * @param wonderful
+ * @param favorable
+ * @param crucial
+ */
+
+/**
+ * @description Some books are not for you to (leaf through), but for you to think through.
+ * @param browse
+ * @param appreciate
+ * @param recommend
+ * @param debate
+ */
+
+/**
+ * @description I was (denied) access to my account after I typed in the wrong password three times in a row.
+ * @param abandoned
+ * @param delayed
+ * @param warned
+ * @param refused
+ */
+
+/**
+ * @description Lots of people make long-distance (commuting) to work daily in big cities.
+ * @param contact
+ * @param discussion
+ * @param travel
+ * @param call
+ */
+
+/**
+ * @description Guests at Four Seasons Hotels can enjoy delicious meals served on (fine) china.
+ * @param delicate
+ * @param special
+ * @param shining
+ * @param desirable
+ */
+
+/**
+ * @description The energy value of dried fruits is considerably (in excess of) that of fresh items.
+ * @param short_of
+ * @param equal_to
+ * @param more_than
+ * @param different_from
+ */
+
+/**
+ * @description When disasters like this happen, we (set aside) whatever pretty disagreements we may have.
+ * @param discuss
+ * @param retain
+ * @param reveal
+ * @param dismiss
+ */
---
+++ src/libs/years/2023.js
@@ -0,0 +1,78 @@
+/**
+ * @description Everything I have learned from history books (affirms) my respect for this great figure.
+ * @param constitutes
+ * @param extends
+ * @param confirms
+ * @param reveals
+ */
+
+/**
+ * @description I fully agree with what the previous speaker has said, but I would like to add a few (remarks) of my own.
+ * @param opinions
+ * @param proposals
+ * @param concepts
+ * @param lessons
+ */
+
+/**
+ * @description His most appealing (trait) is his unfailing sense of humor.
+ * @param hobby
+ * @param feature
+ * @param trace
+ * @param power
+ */
+
+/**
+ * @description Some people feel that using artificial intelligence in this (context) is particularly risky.
+ * @param position
+ * @param situation
+ * @param frame
+ * @param sphere
+ */
+/**
+ * @description All classes across the area will have to be (called off) when the hurricane arrives.
+ * @param restricted
+ * @param postponed
+ * @param canceled
+ * @param abandoned
+ */
+
+/**
+ * @description These books were so old and (fragile) that access to them was limited.
+ * @param valuable
+ * @param delicate
+ * @param weird
+ * @param terrific
+ */
+
+/**
+ * @description Don’t (give out) any personal information about your identity on an unfamiliar app.
+ * @param disclose
+ * @param emit
+ * @param distribute
+ * @param evoke
+ */
+
+/**
+ * @description The issue of (liability) in a driverless car accident has yet to be resolved.
+ * @param flexibility
+ * @param stability
+ * @param security
+ * @param responsibility
+ */
+
+/**
+ * @description The cook will (coat) the pan with some oil first, so the onions don’t stick.
+ * @param fill
+ * @param feed
+ * @param cover
+ * @param circle
+ */
+
+/**
+ * @description Scholars are (inclined) to the view that the research report was composed by someone else.
+ * @param leaning
+ * @param loading
+ * @param leaping
+ * @param leaving
+ */
