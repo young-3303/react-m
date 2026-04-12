4295bd8243104c3199b22e5f7d372c28e1194280
young <55731744@qq.com>
2026-04-12 23:19:08 +0800

    新增2025.js文件，包含多个描述和参数示例

---
+++ src/libs/years/2025.js
@@ -0,0 +1,79 @@
+/**
+ * @description The airline (suspend) flights to the island due to the volcanic ash clouds around the airport.
+ * @param halted
+ * @param managed
+ * @param sustained
+ * @param returned
+ */
+
+/**
+ * @description Scientists collaborated to (crack) the secret of a rare plant species, leading to a groundbreaking discovery.
+ * @param protect
+ * @param keep
+ * @param uncover
+ * @param hide
+ */
+
+/**
+ * @description The reader responded positively to the book’s exploration of new (realms).
+ * @param challenges
+ * @param areas
+ * @param trends
+ * @param concession
+ */
+
+/**
+ * @description The candidate’s (blunder) during the debate cost him the election.
+ * @param strategy
+ * @param behavior
+ * @param mistake
+ * @param concession
+ */
+
+/**
+ * @description Only when you’ve got enough credits will you be (eligible) to apply for the degree.
+ * @param liable
+ * @param available
+ * @param qualified
+ * @param prepared
+ */
+
+/**
+ * @description Experts will be called in to (assess) the environmental impact of the new construction project.
+ * @param evaluate
+ * @param monitor
+ * @param understand
+ * @param alleviate
+ */
+
+/**
+ * @description His campaign started modifying the logo to (appeal to) different types of voters.
+ * @param rally
+ * @param represent
+ * @param register
+ * @param attract
+ */
+
+/**
+ * @description The tax increase was postponed amid worries of a (slump) in consumer spending.
+ * @param doubt
+ * @param guilt
+ * @param decrease
+ * @param restriction
+ */
+
+/**
+ * @description It is vital for students to understand how to (put into practice) the knowledge in real world situations.
+ * @param apply
+ * @param learn
+ * @param obtain
+ * @param seek
+ */
+
+/**
+ * @description (Periodic) checks of the cooling system are necessary to maintain optimal engine performance.
+ * @param Quick
+ * @param Thorough
+ * @param Constant
+ * @param Regular
+ */
